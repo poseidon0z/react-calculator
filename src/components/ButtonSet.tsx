f4bfd0ed2c4d0b9982c86e5c873a838e2c0d93b9
poseidon0z <adityaprabhu1290@gmail.com>
2023-07-13 23:38:09 +0530

    Improved look, and made calculator work

--- src/components/ButtonSet.tsx
+++ src/components/ButtonSet.tsx
@@ -3,14 +3,15 @@ import Button from "./Button";
 interface Props {
   btns: string[];
   onClick: (num: string) => void;
+  extraType?: string;
 }
-function ButtonSet({ btns, onClick }: Props) {
+function ButtonSet({ btns, onClick, extraType = "" }: Props) {
   return (
-    <div className="grid-container">
+    <div className={"grid-container " + extraType}>
       {btns.map((num) => (
         <Button
           key={num}
-          type="btn-primary grid-item"
+          type="btn-primary grid-item "
           onPress={() => {
             onClick(num);
           }}
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,18 +1,32 @@
 import Display from "./Display";
 import ButtonSet from "./ButtonSet";
+import { useState } from "react";
 
 function Calculator() {
-  const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  const syms = ["+", "-", "×", "÷"];
+  const nums = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
+  const syms = ["÷", "×", "-", "C", "+", "="];
+
+  const [expression, setExpression] = useState("");
+
   const onClick = (key: string) => {
-    console.log(key);
+    if (key != "=" && key != "C") {
+      setExpression(expression + key);
+    } else if (key == "C") {
+      setExpression("");
+    } else {
+      const exp = expression.replace("×", "*").replace("÷", "/");
+      const ans = eval(exp);
+      setExpression(expression + " = " + ans);
+      console.log("calculated " + exp + " to be " + ans);
+    }
   };
+
   return (
     <div className="calculator">
-      <Display>Text</Display>
+      <Display>{expression == "" ? "Click buttons" : expression}</Display>
       <div className="flexbox">
         <ButtonSet btns={nums} onClick={onClick} />
-        <ButtonSet btns={syms} onClick={onClick} />
+        <ButtonSet btns={syms} onClick={onClick} extraType="colflex" />
       </div>
     </div>
   );
